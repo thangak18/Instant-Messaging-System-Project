43b9b53ae0bf8e694ba7701621fad8fa0fccac36
hungtmh <hungtmh20002@gmail.com>
2025-10-27 16:34:26 +0700

    refactor(Hùng): Xóa bớt file không cần thiết

--- run_admin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script chạy ứng dụng quản trị
-
-echo "Đang khởi động ứng dụng quản trị..."
-cd /Users/nguyenphunghoangnhi/Downloads/HeThongChat
-javac src/admin/gui/*.java
-java -cp src/admin/gui AdminMainFrame
